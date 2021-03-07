d84c74a3f86a837f8ecd689f7ef51fbbe8ea317e
mateus <mateusmecunhe@gmail.com>
2021-03-07 19:53:13 -0300

    serializing

--- ecommerce_acessivel/app.py
+++ ecommerce_acessivel/app.py
@@ -5,6 +5,9 @@ from scrapy_utils import get_spider_output
 from ecommerce_acessivel.spiders.americanas_catalogo import AmericanasCatalogSpider
 from ecommerce_acessivel.spiders.americanas_produto import AmericanasProductSpider
 from ecommerce_acessivel.serializers import (
+    serialize_specifications,
+    serialize_product_id,
+    serialize_description,
     serialize_specifications
 )
 
@@ -15,6 +18,9 @@ app.config.from_object(settings_dev)
 @app.route('/')
 def catalogo():
     products = get_spider_output(AmericanasCatalogSpider)
+    for p in products:
+        p['product_id'] = serialize_product_id(p['product_id'])
+
     
     return render_template('catalogo.html', products=products)
 
@@ -24,6 +30,8 @@ def produto(product_id):
     
     try:
         product = get_spider_output(AmericanasProductSpider, product_id=product_id)[0]
+        product['description'] = serialize_description(product['description'])
+        product['specifications'] = serialize_specifications(product['specifications'])
         return render_template('produto.html', product=product)
     except:
         return render_template('erro.html')
